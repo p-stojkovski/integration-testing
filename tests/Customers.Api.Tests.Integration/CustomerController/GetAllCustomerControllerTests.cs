432a6f60033fac17a5144f49715e2e02807cc48f
Petar Stojkovski <petars089@gmail.com>
2024-06-24 18:55:08 +0200

    Added Get All Customer Controller Tests

---
+++ tests/Customers.Api.Tests.Integration/CustomerController/GetAllCustomerControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Customers.Api.Tests.Integration.CustomerController;
+
+public class GetAllCustomerControllerTests : IClassFixture<CustomerApiFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.Email, f => f.Person.Email)
+        .RuleFor(x => x.FullName, f => f.Person.FullName)
+        .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGitHubUser)
+        .RuleFor(x => x.DateOfBirth, f => f.Person.DateOfBirth.Date);
+
+    public GetAllCustomerControllerTests(CustomerApiFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnsAllCustomers_WhenCustomersExists()
+    {
+        //Arrange
+        var customer = _customerGenerator.Generate();
+        var createdResponse = await _httpClient.PostAsJsonAsync("customers", customer);
+        var createdCustomer = await createdResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+
+        //Act
+        var response = await _httpClient.GetAsync("customers");
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var customersResponse = await response.Content.ReadFromJsonAsync<GetAllCustomersResponse>();
+        customersResponse!.Customers.Single().Should().BeEquivalentTo(createdCustomer);
+
+        //Cleanup
+        await _httpClient.DeleteAsync($"customers/{createdCustomer!.Id}");
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnsEmptyResponse_WhenNoCustomersExists()
+    {
+        //Act
+        var response = await _httpClient.GetAsync("customers");
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var customersResponse = await response.Content.ReadFromJsonAsync<GetAllCustomersResponse>();
+        customersResponse!.Customers.Should().BeEmpty();
+    }
+}
