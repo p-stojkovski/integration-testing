05e416a6aede20a3b356e6c516ec0c8f8e1125ef
Petar Stojkovski <petars089@gmail.com>
2024-06-24 14:11:27 +0200

    Added integration test to validate creation of customer when email is invalid and github user does not exists

--- tests/Customers.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
+++ tests/Customers.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
@@ -2,6 +2,7 @@
 using Customers.Api.Contracts.Requests;
 using Customers.Api.Contracts.Responses;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http.Json;
 
@@ -40,5 +41,49 @@ public class CreateCustomerControllerTests : IClassFixture<CustomerApiFactory>
         response.StatusCode.Should().Be(HttpStatusCode.Created);
         response.Headers.Location!.ToString().Should()
             .Be($"http://localhost/customers/{customerResponse!.Id}");
+    }
+
+    [Fact]
+    public async Task Create_ReturnsValidationError_WhenEmailIsInValid()
+    {
+        //Arrange
+        var invalidEmail = "fsdfsf121";
+        var customer = _customerGenerator
+            .Clone()
+            .RuleFor(x => x.Email, invalidEmail)
+            .Generate();
+
+        //Act
+        var response = await _httpClient.PostAsJsonAsync("customers", customer);
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        
+        var error = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        error!.Status.Should().Be(400);
+        error.Title.Should().Be("One or more validation errors occurred.");
+        error.Errors["Email"][0].Should().Be($"{invalidEmail} is not a valid email address");
+    }
+
+    [Fact]
+    public async Task Create_ReturnsValidationError_WhenGitHubUserDoesNotExists()
+    {
+        //Arrange
+        var invalidGitHubUser = "fsdfsf121";
+        var customer = _customerGenerator
+            .Clone()
+            .RuleFor(x => x.GitHubUsername, invalidGitHubUser)
+            .Generate();
+
+        //Act
+        var response = await _httpClient.PostAsJsonAsync("customers", customer);
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var error = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        error!.Status.Should().Be(400);
+        error.Title.Should().Be("One or more validation errors occurred.");
+        error.Errors["Customer"][0].Should().Be($"There is no GitHub user with username {invalidGitHubUser}");
     }
 }
